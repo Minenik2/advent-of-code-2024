f7b7cca77c22a1833301515d98d21eb9b1c1908a
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2024-12-02 23:14:10 +0100

    completed day 2!

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,6 +5,23 @@ safeAmount = 0
 isAscending = False
 isDecending = False
 
+def checkIfSafe(array, isAscending):
+    for i in range(len(array)):
+        if i == 0:
+            continue
+        if int(array[i-1]) < int(array[i]) and isAscending:
+            safe = True
+        elif int(array[i-1]) > int(array[i]) and not(isAscending):
+            safe = True
+        else:
+            return False
+            
+        if abs(int(array[i-1]) - int(array[i])) <= 3 and abs(int(array[i-1]) - int(array[i])) >= 1:
+            safe = True
+        else:
+            return False
+    return safe
+
 for x in f:
     array = x.split()
     
@@ -14,52 +31,27 @@ for x in f:
     else:
         isAscending = False
         
-        
+    safe = checkIfSafe(array, isAscending)
     
-    for i in range(len(array)):
-        if i == 0:
-            continue
-        # check if the array is acending or check if the array is decending
-        if int(array[i - 1]) < int(array[i]) and isAscending:
-            #print(array[i-1], " < ", array[i])
-            safe = True
-        elif int(array[i - 1]) > int(array[i]) and not(isAscending):
-            safe = True
-            #print(array[i-1], " > ", array[i])
-        else:
-            safe = False
-            break
-        
-        if abs(int(array[i - 1]) - int(array[i])) <= 3 and abs(int(array[i - 1]) - int(array[i])) >= 1:
-            safe = True
-        else:
-            safe = False
-            #print("break", array[i - 1],"vs", array[i], " is ",  abs(int(array[i - 1]) - int(array[i])), "apart")
-            break
     # if its not safe
     if not(safe):
         for i in range(len(array)):
-            currentArray = array
-            currentArray.pop(i)
-            checkIfSafe(int(array[i - 1]), int(array[i]), isAscending)
+            currentArray = array.copy()
+            removed = currentArray.pop(i)
+            if (i == 0) or (i == 1):
+                if int(currentArray[0]) < int(currentArray[1]):
+                    isAscending = True
+                else:
+                    isAscending = False
+            safe = checkIfSafe(currentArray, isAscending)
+            if safe: break
             
     #print(array, " is safe: ", safe, safeAmount)
     if safe:
         safeAmount += 1
         
         
-def checkIfSafe(a, b, isAscending):
-    if int(a) < int(b) and isAscending:
-        safe = True
-    elif int(a) > int(b) and not(isAscending):
-        safe = True
-    else:
-        return False
-        
-    if abs(int(a) - int(b)) <= 3 and abs(int(a) - int(b)) >= 1:
-        return safe
-    else:
-        return False
+
         
         
 print(safeAmount)
