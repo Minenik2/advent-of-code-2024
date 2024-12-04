5dba18e16cf6130b05a13513e8a123bc1b2a2779
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2024-12-04 01:17:33 +0100

    solved day3!

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,6 +1,8 @@
 f = open("Day3\day3.txt", "r")
 
+do = True
 mulRequirment = 0
+doRequirment = 0
 leftNumer = ""
 rightNumber = ""
 result = 0
@@ -10,16 +12,37 @@ def mul(a, b):
 
 for line in f:
     for c in line:
-        if c == "m" and mulRequirment == 0:
+        if c == "d" and doRequirment == 0:
+            doRequirment += 1
+        elif c == "o" and doRequirment == 1:
+            doRequirment += 1
+        elif c == "n" and doRequirment == 2:
+            doRequirment += 1
+        elif c == "'" and doRequirment == 3:
+            doRequirment += 1
+        elif c == "t" and doRequirment == 4:
+            doRequirment += 1
+        elif c == "(" and doRequirment == 5:
+            doRequirment += 1
+        elif c == ")" and doRequirment == 6:
+            do = False
+        elif c == "(" and doRequirment == 2:
+            doRequirment += 1
+        elif c == ")" and doRequirment == 3:
+            do = True
+        else:
+            doRequirment = 0
+        
+        if c == "m" and mulRequirment == 0 and do:
             mulRequirment += 1
-        elif c == "u" and mulRequirment == 1:
+        elif c == "u" and mulRequirment == 1 and do:
             mulRequirment += 1
-        elif c == "l" and mulRequirment == 2:
+        elif c == "l" and mulRequirment == 2 and do:
             mulRequirment += 1
-        elif c == "(" and mulRequirment == 3:
+        elif c == "(" and mulRequirment == 3 and do:
             mulRequirment += 1
             leftNumer = ""
-        elif mulRequirment == 4 and c != ",":
+        elif mulRequirment == 4 and c != "," and do:
             try:
                 int(c)
             except ValueError:
@@ -29,10 +52,10 @@ for line in f:
                 leftNumer = leftNumer + c
             else:
                 mulRequirment = 0
-        elif mulRequirment == 4 and c == ",":
+        elif mulRequirment == 4 and c == "," and do:
             mulRequirment += 1
             rightNumber = ""
-        elif mulRequirment == 5 and c != ")":
+        elif mulRequirment == 5 and c != ")" and do:
             try:
                 int(c)
             except ValueError:
@@ -42,7 +65,7 @@ for line in f:
                 print("right number is now:", rightNumber)
             else:
                 mulRequirment = 0
-        elif mulRequirment == 5 and c == ")":
+        elif mulRequirment == 5 and c == ")" and do:
             mulRequirment = 0
             result += mul(leftNumer, rightNumber)
             print("multiplying!", leftNumer, rightNumber)
